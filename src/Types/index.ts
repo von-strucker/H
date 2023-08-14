bd18c84378e17c4aee0fe257ebbb49002a2b45d4
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-08-14 11:07:47 +0200

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -54,7 +54,8 @@ export interface YT_Search {
 export enum GroupFeatures {
     'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
     'mods' = "By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bot's an admin",
-    'nsfw' = 'By enabling this feature, it enables the bot to send *NSFW* contents'
+    'nsfw' = 'By enabling this feature, it enables the bot to send *NSFW* contents',
+    'badword' = 'Jika diaktifkan, semua chat didalam grup ini akan difilter, jika terdapat pesan yang mengandung kata kasar, orang yang mengirimnya akan dibanned'
 }
 
 export interface IGroup extends GroupMetadata {
