70f6fa601262731803088bc848ac28098a79563e
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-08-14 11:50:25 +0200

    Update Message.ts

--- src/Handlers/Message.ts
+++ src/Handlers/Message.ts
@@ -5,7 +5,7 @@ import { Message, Client, BaseCommand } from '../Structures'
 import { getStats } from '../lib'
 import { ICommand, IArgs } from '../Types'
 import axios from 'axios'
-import FindBadword from './FIndBadword'
+import FindBadword from './FindBadword'
 
 export class MessageHandler {
     constructor(private client: Client) {}
