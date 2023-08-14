6d7156efdba580c95b67f3cab9723d34fea2cf18
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-08-14 12:52:17 +0200

    Update FindBadword.ts

--- src/Handlers/FindBadword.ts
+++ src/Handlers/FindBadword.ts
@@ -16,6 +16,19 @@ export default class FindBadword {
   public execute = async (M: Message): Promise<void> => {
     const user = await this.database.findUser(M.sender.jid)
 
+    if (user) {
+      let badwordUser = user.badwordCount
+      const messages =
+        M.message.message?.conversation ||
+        M.message.message?.extendedTextMessage?.text
+
+      let containBadword = false
+      for (const msg of messages.split(' ')) {
+        if (this.badword.indexOf(msg.toLowerCase()) > -1) {
+          containBadword = true
+        }
+      }
+
       if (containBadword && badwordUser !== this.maxBadWord) {
         badwordUser += 1
         await this.database.updateUser(
@@ -42,4 +55,5 @@ export default class FindBadword {
       }
     }
   }
-}
+ }
+            
